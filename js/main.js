0f3bff19f66b29024687e3d0aa196816de1a047b
gloriafercu <gloriafercu@gmail.com>
2018-04-07 02:23:24 +0200

    add ingredients to shopping-cart js

--- js/main.js
+++ js/main.js
@@ -1,3 +1,45 @@
 'use strict';
 
-console.log('>> Ready :)');
+/* Petición a la API para rellenar los campos de la shopping__cart */
+
+fetch('https://raw.githubusercontent.com/Adalab/recipes-data/master/rissoto-setas.json')
+  .then(function(response) {
+    return response.json();
+  })
+  .then(function(json) {
+
+		// Nombre de la receta
+		var recipe = json.recipe;
+		var nameRecipe = document.querySelector('.name__recipe');
+		nameRecipe.innerHTML = recipe.name;
+
+		// Ingredientes
+		var ingredients = recipe.ingredients;
+		var listHTML = '';
+		for (var i = 0; i < ingredients.length; i++) {
+			listHTML +=
+			'<li class="article">' +
+				'<label for="product' + (i+1) + '">' +
+					'<div class="article__block1">' +
+						'<input type="checkbox" name="product" id="product' + (i+1) + '"  class="checkbox"/>' +
+						'<input type="number" class="quantity" name="quantity" min="0" max="10" value="1"/>' +
+						'<div class="group">' +
+							'<span class="product">' + ingredients[i].product + '</span>' +
+							'<span class="brand">' + (ingredients[i].brand ? ingredients[i].brand : '') + '</span>' +
+							'<span class="items">' + ingredients[i].quantity + '</span>' +
+						'</div>' +
+					'</div>' +
+					'<div class="article__block2">' +
+						'<span class="price">' + ingredients[i].price + ' ' + recipe.currency + '</span>' +
+					'</div>' +
+				'</label>' +
+			'</li>';
+		}
+		var ulArticles = document.querySelector('.articles');
+		ulArticles.innerHTML = listHTML;
+
+		// Costes de envio
+		var shippingCost = recipe["shipping-cost"];
+		var shippingPrice = document.querySelector('.shipping__price');
+		shippingPrice.innerHTML = shippingCost + ' ' + recipe.currency;
+  });
